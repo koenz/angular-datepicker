14d7db751f138c9b06b9b2ace316eb295380dafc
Zigterman, Koen <Koen.Zigterman@aperto.com>
2018-12-02 17:39:27 +0100

    Update to angular 6

---
+++ projects/ngx-animating-datepicker/src/public_api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of ngx-animating-datepicker
+ */
+export { Options } from './lib/models/datepicker-options.model';
+export { DirectiveOptions } from './lib/models/directive-options.model';
+export { AaDatepickerModule } from './lib/ngx-animating-datepicker.module';
+export { DatepickerComponent } from './lib/components/datepicker/datepicker.component';
+export { AnimatepickerComponent } from './lib/components/animatepicker/animatepicker.component';
